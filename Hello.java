1f6b54c33b47c0d4f3c5b9b811c488b6ad24d725
Munindar Kumar <munindaralw@gmail.com>
2017-10-26 11:30:22 +0530

    added one more method sayHello()
    also changed hello string in main()

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello
 {
   public static void main(String args[]){
-    System.out.println("Hello");
+    System.out.println("Hello From branch1");
+  }
+  public sttatic void sayHello(){
+    System.out.println("Hello From new method");
   }
 }
